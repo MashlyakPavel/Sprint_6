47336361b932574d2119e2577c1ab4d64b2b9808
Pavel Mashlyak <mpainfin@gmail.com>
2024-10-10 17:42:25 +0300

    bug fixes

--- src/test/java/LionParameterizedTest.java
+++ src/test/java/LionParameterizedTest.java
@@ -8,25 +8,32 @@ import static org.junit.Assert.assertEquals;
 // Параметризованный тест
 @RunWith(Parameterized.class)
 public class LionParameterizedTest {
-    private final String sex;
-    private final boolean expectedHasMane;
 
+    // Поля для хранения данных параметров
+    private final String sex; // Пол
+    private final boolean expectedHasMane; // Ожидаемое наличие гривы
+
+    // Конструктор для инициализации параметров
     public LionParameterizedTest(String sex, boolean expectedHasMane) {
         this.sex = sex;
         this.expectedHasMane = expectedHasMane;
     }
 
+    // Метод возвращающий данные для параметризованных тестов
     @Parameterized.Parameters
     public static Object[][] getData() {
         return new Object[][]{
-                {"Самец", true},
-                {"Самка", false}
+                {"Самец", true},  // Для самца ожидается наличие гривы
+                {"Самка", false}  // Для самки ожидается отсутствие гривы
         };
     }
 
     @Test
     public void testDoesHaveMane() throws Exception {
+        // Создаем экземпляр класса Lion с заданным полом
         Lion lion = new Lion(sex, new Feline());
+
+        // Проверяем соответствует ли результат ожиданиям
         assertEquals(expectedHasMane, lion.doesHaveMane());
     }
 }
